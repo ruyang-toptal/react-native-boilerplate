86a1d3534b697558e1aad6ca1fea6c3b6354595e
ruyang-toptal <ruyang@toptal.com>
2017-03-02 07:04:14 +0800

    codepush test

--- app/components/DemoView/DemoView.js
+++ app/components/DemoView/DemoView.js
@@ -47,7 +47,7 @@ export default class DemoView extends Component {
         <Text style={styles.conclusion}>
         {`${this.state.name} likes ${this.state.favorite || "___"} because ${this.state.reason || "___"}.`}
         </Text>
-        <Text>I recommend apple.</Text>
+        <Text>He likes banana</Text>
         <Text>Because apple a day keeps doctor away.</Text>
       </View>
     );
